ec0d9ad7894030bf96c8bd72692384d8ef26abcf
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-28 14:15:09 -0700

    refactored for global testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,40 +2,14 @@
 // 🚨🚨 https://mswjs.io/docs/ 🚨🚨
 
 import { screen, render } from '@testing-library/react'
+import { rest } from 'msw'
+import { server } from './setupTests'
 // 🚨
 // import rest
 // import setupServer
-import { setupServer } from 'msw/node'
-import { rest } from 'msw'
+
 import App from './App'
-
-const user = {
-  id: 1,
-  created_at: '2021-12-13T00:17:29+00:00',
-  // 🚨 Add a name here
-  name: 'Vonta',
-  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-  motto: 'Res Non Verba',
-  color: 'crimson',
-}
-
-// 🚨 Create your server
-const server = setupServer(
-  rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`, (req, res, ctx) =>
-    res(ctx.json([user]))
-  )
-)
-// 🚨 Listen for server start
-beforeAll(() => server.listen())
-
-afterEach(() => {
-  server.resetHandlers()
-})
-
-// 🚨 Close server when complete
-afterAll(() => server.close())
+import { user, sasuke } from './fixtures/characterData'
 
 test('Should render the header', async () => {
   render(<App />)
@@ -51,18 +25,6 @@ test('Should render the header', async () => {
 })
 
 test('Should render the header with Sasuke 🌬️🔥', async () => {
-  const sasuke = {
-    id: 1,
-    created_at: '2021-12-13T00:17:29+00:00',
-    name: 'Sasuke 🌬️🔥',
-    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-    motto: 'Res Non Verba',
-    color: 'crimson',
-  }
-
-  // 🚨 Use the server to change the response for this test
   server.use(
     rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`, (req, res, ctx) =>
       res(ctx.json([sasuke]))
